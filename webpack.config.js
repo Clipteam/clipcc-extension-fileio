9ebe4e59dd27a020e1dd0474eb65d61b813d3fa8
SinanGentoo <59327348+SinanGentoo@users.noreply.github.com>
2022-02-07 20:19:10 +0800

    Adapting to the latest extension loading method (#2)

--- webpack.config.js
+++ webpack.config.js
@@ -4,26 +4,37 @@ const ZipWebpackPlugin = require('zip-webpack-plugin');
 
 const info = require('./src/info.json');
 
-module.exports = {
+const config = {
     mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
     entry: './src/index.js',
     output: {
         filename: 'main.js',
-        path: path.resolve(__dirname, 'build')
+        path: path.resolve(__dirname, 'build'),
+        module: true,
+        library: {
+            type: 'commonjs2'
+        }
+    },
+    experiments: {
+        outputModule: true
     },
     externals: {
         'clipcc-extension': 'ClipCCExtension',
         'electron': 'commonjs electron',
         'fs': 'commonjs fs'
     },
+    externalsType: 'global',
     plugins: [
         new CopyWebpackPlugin({
             patterns: [{
-                from: path.join(__dirname, 'src'),
-                to: path.join(__dirname, 'build'),
-                globOptions: {
-                    ignore: ['**/*.js']
-                }
+                from: path.join(__dirname, 'src/locales'),
+                to: path.join(__dirname, 'build/locales')
+            }, {
+                from: path.join(__dirname, 'src/assets'),
+                to: path.join(__dirname, 'build/assets')
+            }, {
+                from: path.join(__dirname, 'src/info.json'),
+                to: path.join(__dirname, 'build/info.json')
             }]
         }),
         new ZipWebpackPlugin({
@@ -33,3 +44,5 @@ module.exports = {
         })
     ]
 };
+
+module.exports = config;
